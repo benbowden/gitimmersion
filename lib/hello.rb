12db0ed0f63b20b988c4f3a4175000d5edc5a01a
benbowden <bbowden9@uncc.edu>
2015-11-06 05:12:45 +0000

    Changes

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require 'greeter'
 
 # Default is World
@@ -6,9 +5,4 @@ name = ARGV.first || "World"
 
 greeter = Greeter.new(name)
 puts greeter.greet
-=======
-puts "What's your name"
-my_name = gets.strip
 
-puts "Hello, #{my_name}!"
->>>>>>> master
